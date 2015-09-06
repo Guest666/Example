b20369ec55cd97a5ddbbfde3b2b426516115a51a
Guest666 <koveringh@gmail.com>
2015-09-06 19:41:59 +0300

    Init_expml
    comment init example

---
+++ Example/Example.cpp
@@ -0,0 +1,20 @@
+// Example.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+
+
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a;
+	cout << "Teståå: " << "\n";
+	cin >> a;
+	return 0;
+}
+
